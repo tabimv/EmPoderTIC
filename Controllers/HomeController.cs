e9ff98c1fce5a280eada1409044a7850f58edb55
tabimv <ta.melo@duocuc.cl>
2023-11-10 14:49:05 -0300

    Página Inicio EmPoderTIC Por: Ignacio Selman

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using EmPoderTIC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmPoderTIC.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private EmPoderTICConexionFinal db = new EmPoderTICConexionFinal();
+
+        // GET: 
+        public ActionResult InicioGeneral()
+        {
+            return View();
+        }
+    }
+}
