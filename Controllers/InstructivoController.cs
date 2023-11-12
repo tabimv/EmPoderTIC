0d7a0ca2690e7b8957bcd1cfd0e50cd386e3b9db
tabimv <ta.melo@duocuc.cl>
2023-11-12 20:36:06 -0300

    Instructivo para Panel Administración

---
+++ Controllers/InstructivoController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EmPoderTIC.Controllers
+{
+    public class InstructivoController : Controller
+    {
+        // GET: Instructivo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult PortalBadges()
+        {
+            return View();
+        }
+    }
+}
